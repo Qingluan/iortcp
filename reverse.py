4731260b554333e8e457ac96b3c838a739246555
root <root@localhost.localdomain>
2019-06-28 13:25:09 -0400

    ok

--- reverse.py
+++ reverse.py
@@ -1,15 +1,19 @@
 #!/bin/python
 # from argparse import ArgumentParser
 import socket
+import traceback
 from socket import SO_REUSEADDR, SO_REUSEPORT, SOL_SOCKET
 from socket import AF_INET, SOCK_STREAM
 import sys 
 import select
 import time
+import argparse
 # import pdb
-if sys.version[0] == '2':
-    class ConnectionResetError(Exception):pass
-    class ConnectionRefusedError(Exception):pass
+PY = int(sys.version[0])
+if PY == 2:
+    class ConnectionResetError(socket.error):pass
+    class ConnectionRefusedError(socket.error):pass
+    class BrokenPipeError(socket.error):pass
 
 REVERSE_OP = b'[{_m0re_c0n_}]'
 
@@ -31,7 +35,10 @@ def create_server(port):
         yield s
 
 def log(i, end='\n'):
-    sys.stdout.write(i+end)
+    if PY ==3 and isinstance(i,bytes):
+        sys.stdout.write(str(i)+end)
+    else:
+        sys.stdout.write(i+end)
     sys.stdout.flush()
 
 class SenderHandler:
@@ -46,11 +53,16 @@ class SenderHandler:
             self.lsock = None
         self.laddr = laddr
         self.addr = addr
+        if not addr:
+            self.addr = 'localhost:12364'
+        if not laddr:
+            self.laddr = 'localhost:12365'
         self.to_ldata = b''
         self.to_rdata = b''
         self.unconnect = ''
         self.reset = False
         self._alive_interval = alive_check_sec
+        self.start_time = time.time()
         self._time = time.time()
         self._now_handle_socks = {}
 
@@ -87,7 +99,7 @@ class SenderHandler:
             del self._now_handle_socks[k]
 
     def handle(self):
-        # n = time.time()
+        self._ = time.time()
         # if n - self._time > self._alive_interval:
             # self.check_alive(self.sock)
             # self._time = n
@@ -108,8 +120,13 @@ class SenderHandler:
         for s in rl:
             self.handle_read(s)
         
-        if self.to_ldata or self.to_rdata:
-            for s in wl:
+        #log("data: r[%s] l[%s]" % (self.to_rdata, self.to_ldata) ,end='\r')
+        for s in wl:
+            if len(self.to_rdata) > 0 and s is self.sock:
+                #log(" --> local")
+                self.handle_write(s)
+            if len(self.to_ldata) > 0 and s is self.lsock:
+                #log(" --> remote")
                 self.handle_write(s)
 
         self.clean
@@ -117,9 +134,15 @@ class SenderHandler:
 
     def handle_error(self, sock):
         sock.close()
+        self.unregist_sock(sock)
+        """if sock != self.lsock and self.lsock:
+            self.lsock.close()
+            self.unregist_sock(self.lsock)"""
         if sock != self.lsock and self.lsock:
             self.lsock.close()
             self.unregist_sock(self.lsock)
+        #else:
+        #    self.unregist_sock(self.sock)
         self.reset = True
         self.unregist_sender()
 
@@ -129,7 +152,7 @@ class SenderHandler:
         
         SenderHandler(addr=self.addr, laddr=self.laddr)
         SenderHandler(addr=self.addr, laddr=self.laddr)
-        log("reverse connection: %d"% len(self.__class__.all_sender))
+        log("reverse connection: %d"% len(self.__class__.all_sender), end='\r')
         return d
 
     def handle_read(self, sock):
@@ -144,62 +167,78 @@ class SenderHandler:
             self.handle_error(sock)
             return
         if sock == self.sock:
-            log("from : %s | %d" %(self.addr, len(d)))
             if REVERSE_OP in d:
+                #log("from : %s " %(self.addr))
                 d = self.handle_build_more(d)
+                #import pdb;pdb.set_trace()
                 if not d:
                     return
             self.to_ldata += d
             if not self.lsock:
                 self.lsock = self.create_sock(self.laddr)
                 self.regist_sock(self.lsock)
-        
         elif sock == self.lsock:
-            log("from : %s" %self.laddr)
+            #log("from : %s" %self.laddr)
             self.to_rdata += d
 
+        #log("data:[%s] [%s] [%s]" % (d,self.to_rdata, self.to_ldata) )
     
         
 
     def handle_write(self, sock):
         dirty = False
         try:
-            if sock == self.sock:
+            if sock == self.sock and self.to_rdata:
                 sn = sock.send(self.to_rdata)
                 # print(self.to_rdata)
-                log("reverse <- %d " % sn, end='\r')
+                #log(self.to_rdata)
+                #log("reverse <- %d " % sn, end='\n')
 
                 self.to_rdata = self.to_rdata[sn:]
                 if self.to_rdata:
                     dirty = True
-            elif sock == self.lsock:
+            elif sock == self.lsock and self.to_ldata:
                 sn = sock.send(self.to_ldata)
                 # print(self.to_ldata)
-                log("reverse -> %d  " % sn, end='\r')
+                #log(self.to_ldata)
+                #log("reverse -> %d  " % sn, end='\n')
                 self.to_ldata = self.to_ldata[sn:]
                 if self.to_ldata:
                     dirty = True
             
-        except BrokenPipeError:
+        except BrokenPipeError as e:
             log("socks break ! . reset")
             self.handle_error(sock)
             dirty = False
+        except socket.error as e:
+            if e.errno == socket.errno.EBADF:
+                log("socks break ! . reset")
+            else:
+                log("sockset: %s " % e)
+            self.handle_error(sock)
+            dirty = False
+        except Exception as e:
+            log("0999")
 
-        if dirty:
-            self.handle_write(sock)
+
+        #if dirty:
+        #    self.handle_write(sock)
 
     def check_alive(self, sock):
         r,w, _ = select.select([sock], [], [], 1)
         if len(r) > 0:
             d = r[0].recv(9216)
             if not d:
+                log("check alive [x] ")
+                self.handle_error(sock)
                 return False
             if sock == self.sock:
                 self.to_ldata += d
             else:
                 self.to_rdata += d
 
-    def create_sock(self, addr, block=False):
+    def create_sock(self, addr, block=False, try_time=4):
+        #log("build sock: %s" %addr)
         if isinstance(addr, str):
             if ':' in addr:
                 ip,port = addr.split(":")
@@ -215,8 +254,16 @@ class SenderHandler:
             port = addr[1]
         
         sock = socket.socket(AF_INET, SOCK_STREAM)
-        sock.connect((ip, port))
-        # log('%s:%d --> ok' % (ip,port), end='\r')
+        try:
+            sock.connect((ip, port))
+        except socket.gaierror as e:
+            log("try conn -> %s:%d [x]"%(ip, port))
+            if try_time == 0:
+                log("[261] :%s" % e)
+                raise e
+            else:
+                return self.create_sock(addr,block,try_time=try_time-1)
+
         sock.setblocking(block)
         return sock
 
@@ -284,17 +331,20 @@ class WaiterHandler(SenderHandler):
                 # bc += 1
                 hand.handle()
                 if hand.reset:
-                    raise ConnectionResetError("break")
-            except ConnectionResetError:
-                log("connection break %r" % hand)
-                un_r.append(hand)
+                    log("connection break %r" % hand)
+                    un_r.append(hand)
+            except socket.error as e:
+                if socket.errno.EBADF != e.errno:
+                    log("[337] %s " % e)
+
+
         
         # clear break connection
         for u in un_r:
             self._runing_handlers.remove(u)
 
         if need_conn_num > 0 and len(self._runing_handlers) > 0:
-            log("send cmd to create more conec")
+            #log("send cmd to create more conec", end='\r')
             h = self._runing_handlers[0]
             h.to_ldata += REVERSE_OP
             h.handle_write(h.lsock)
@@ -305,6 +355,7 @@ class WaiterHandler(SenderHandler):
         for s in rl:
             d = s.recv(1024)
             if not d:
+                self.handle_error(s)
                 self.recv_list.remove(s)
             else:
                 log("error: %s"% d)
@@ -338,23 +389,31 @@ def ConnectServer(lport, rport):
     ic = 0
     
     R = WaiterHandler(lport, rport)
-    while 1:        
-        ic +=1
-        R.handle()
+    try:
+        while 1: 
+            ic +=1
+            R.handle()
+    except Exception as e:
+        traceback.print_stack()
+            
         # log("Rebuild server: %d"  % ic, end='\r')
         
 
 def StartServer(addr, laddr):
     ic = 0
     bc = 0
+    conn_err_t = 0
     while 1:
         try:
             w = SenderHandler(addr=addr, laddr=laddr)
             # w.handle_build_more(REVERSE_OP)
-        except ConnectionRefusedError:
+            conn_err = 0
+        except ConnectionRefusedError as e:
+            log("[406] %s"% str(e))
             ic += 1
             log("con : error try again : %d" % ic , end='\r')
-            time.sleep(1)
+            time.sleep(1 + conn_err_t % 30)
+            conn_err += 1
             continue
         try:
             ccc = 0
@@ -372,12 +431,47 @@ def StartServer(addr, laddr):
                         
                         continue
                 if not SenderHandler.all_sender:
-                    raise ConnectionResetError()
+                    #raise ConnectionResetError()
+                    continue
         except ConnectionResetError:
             bc += 1
             # log("connection break : %d" % bc, end='\r')
             time.sleep(0.01)
             continue
+
+
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-B', '--build', default='40001:40002', help='build [listen port1] < -- >  [listen port2] exm: port1:port2 ')
+    parser.add_argument('-l', '--local', default=None, help='example: localserver:port1 connect local port must use with -r : connect to remote port ')
+    parser.add_argument('-r', '--remote', default=None, help='example: remoteserver:port2 connect to remote port ')
+    args = parser.parse_args()
+    if args.local:
+        assert args.remote != None
+        if ':' not in args.local:
+            l_port = int(args.local)
+            l = 'localhost:%d' % l_port
+        else:
+            l = args.local
+
+        if ':' in args.remote:
+            r = args.remote
+        else:
+            r_port = int(args.remote)
+            r = 'localhost:%d' % r_port
+        log("put local's port: %s  ---> connect --> %s" % (l,r))
+        StartServer(r.strip(), l.strip())
+
+    else:
+        try:
+            assert ":" in args.build
+            port1,port2 = args.build.split(":")
+            log(" %s  <---> connect <--> %s" % (port1, port2))
+            ConnectServer(int(port1), int(port2))
+        except Exception as e:
+            log("err bye : %s" % str(e) )
+
+
 
 
 def main():
@@ -394,7 +488,7 @@ def main():
             ConnectServer(int(port1), int(port2))
     except Exception as e:
         raise e
-        log("port -> ip:port  or port1:port2")
 
 if __name__ == '__main__':
-    main()
+    get_config()
+    #main()
